d1dd0e29abcb3acc9785c2bc12cc6c168787ade9
antoniopedroniart-ctrl <antoniopedroni.art@gmail.com>
2025-10-16 11:54:38 +0200

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,1 +1,3 @@
 import StudyPlanner from './pages/StudyPlanner';
+<NavLink to="/study" className={({isActive})=> isActive? 'btn primary' : 'btn'}>Study Planner</NavLink>
+<Route path="/study" element={<StudyPlanner/>} />
