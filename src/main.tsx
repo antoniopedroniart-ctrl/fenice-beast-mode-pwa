5f52c560328f46ce0f1f5e96e6d627632a21ce3b
antoniopedroniart-ctrl <antoniopedroni.art@gmail.com>
2025-10-17 08:32:22 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,12 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
-import './styles.css';
-import { registerSW } from './pwa/registerSW';
-
-registerSW();
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
-);
+)
